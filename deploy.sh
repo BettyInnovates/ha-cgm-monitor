fa11bf22972209c27da5ae35497327d0c1ae74d2
BettyInovates <bettymobil2@gmail.com>
2026-03-15 18:45:23 +0100

    init from plant

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 # in windows
 # netzlaufwerk verbinden \\192.168.1.32\config ...
 # in linux
-# sudo mount -t drvfs I: /home/betty/mnt-ha
+# sudo mount -t drvfs I: /mnt/i
 cp custom_components/ha-cgm-monitor /mnt/i/custom_components/ha-cgm-monitor -fR
